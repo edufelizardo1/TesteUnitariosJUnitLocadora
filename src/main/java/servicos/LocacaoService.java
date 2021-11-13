b33a818cf33fc8a3db8741712b4ed5ffef9dc30a
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-11-13 17:12:22 -0300

    Update LocacaoService.java

--- src/main/java/servicos/LocacaoService.java
+++ src/main/java/servicos/LocacaoService.java
@@ -2,17 +2,29 @@ package servicos;
 
 import static utils.DataUtils.adicionarDias;
 
+import java.nio.file.FileAlreadyExistsException;
 import java.util.Date;
 
 import entidades.Filme;
 import entidades.Locacao;
 import entidades.Usuario;
+import exceptions.FilmesSemEstoqueException;
+import exceptions.LocadoraException;
 import utils.DataUtils;
 
 
 public class LocacaoService {
 	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+	public Locacao alugarFilme(Usuario usuario, Filme filme) throws FilmesSemEstoqueException, LocadoraException {
+		if (usuario == null) {
+			throw new LocadoraException("Usuário vazio.");
+		}
+		if (filme == null) {
+			throw new LocadoraException("Filme vazio.");
+		}
+		if (filme.getEstoque() == 0) {
+			throw new FilmesSemEstoqueException();
+		}
 		Locacao locacao = new Locacao();
 		locacao.setFilme(filme);
 		locacao.setUsuario(usuario);
@@ -40,11 +52,16 @@ public class LocacaoService {
 		Filme filme = new Filme("Filme 1", 2, 5.0);
 		
 		//acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//verificacao
-		System.out.println(locacao.getValor() == 5.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		Locacao locacao = null;
+		try {
+			locacao = service.alugarFilme(usuario, filme);
+			//verificacao
+			System.out.println(locacao.getValor() == 5.0);
+			System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+			System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
